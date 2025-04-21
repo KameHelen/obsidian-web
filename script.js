7f2e265dbc44dea8e4f18699480d5f593703bf8f
KameHelen <elemesreq@gmail.com>
2025-04-21 17:57:19 +0200

    añadiendo carrusel

---
+++ script.js
@@ -0,0 +1,19 @@
+<script>
+  const prevBtn = document.querySelector('.carrusel-btn.prev');
+  const nextBtn = document.querySelector('.carrusel-btn.next');
+  const carrusel = document.querySelector('.carrusel');
+
+  prevBtn.addEventListener('click', () => {
+    carrusel.scrollBy({
+      left: -300,
+      behavior: 'smooth'
+    });
+  });
+
+  nextBtn.addEventListener('click', () => {
+    carrusel.scrollBy({
+      left: 300,
+      behavior: 'smooth'
+    });
+  });
+</script>

d1fa33049361cef0e3c5610edac325734724f57a
german <gerferpin@gmail.com>
2025-04-24 19:51:43 +0200

    login del usuario

---
+++ script/login.js
@@ -0,0 +1,13 @@
+document.getElementById("formulario").addEventListener("submit", function (e) {
+    e.preventDefault();  // Evitar que el formulario se envíe de la forma tradicional.
+
+    // Obtener los valores del formulario
+    const nombre = document.getElementById("nombre").value;
+
+    // Guardar los datos en sessionStorage
+    sessionStorage.setItem("nombre", nombre);
+
+
+    // Redirigir a la página de resultado de compra
+    window.location.href = "resultado.html";
+});

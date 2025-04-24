acabe6c9e2ecf394967552efd05f1be47559c89d
Ronnald <benronnald@gamil.com>
2025-04-24 18:26:17 +0200

    SS

--- script/index.js
+++ script/index.js
@@ -1,43 +1,34 @@
-
-    const menuBtn = document.querySelector('.menu-btn');
-    menuBtn.addEventListener('click', () => {
-        menuBtn.classList.toggle('active');
-    });
-
-    // Deslizador de video 
-    const btns = document.querySelectorAll('.nav-btn');
-    const slides = document.querySelectorAll('.video-slide');
-    const contents = document.querySelectorAll('.content');
-
-    let currentIndex = 0;
-
-    var sliderNav = function(manual) {
-        btns.forEach((btn) => {
-            btn.classList.remove('active');
-        });
-        slides.forEach((slide) => {
-            slide.classList.remove('active');
-        });
-        contents.forEach((content) => {
-            content.classList.remove('active');
+        const menuBtn = document.querySelector('.menu-btn');
+        menuBtn.addEventListener('click', () => {
+            menuBtn.classList.toggle('active');
         });
 
-        btns[manual].classList.add('active');
-        slides[manual].classList.add('active');
-        contents[manual].classList.add('active');
+        //deslizador de video 
+        const btns = document.querySelectorAll('.nav-btn');
+        const slides = document.querySelectorAll('.video-slide');
+        const contents = document.querySelectorAll('.content');
+        
+        var sliderNav = function(manual){
+            btns.forEach((btn) => {
+                btn.classList.remove('active');
+            });
+            slides.forEach((slides) => {
+                slides.classList.remove('active');
+            });
+            contents.forEach((contents) => {
+                contents.classList.remove('active');
+            });
+     
+     
+            btns[manual].classList.add('active');
+            slides[manual].classList.add('active');
+            contents[manual].classList.add('active');
 
-        currentIndex = manual; // actualizar el índice actual
-    }
+        }
 
-    btns.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            sliderNav(i);
+        btns.forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                sliderNav(i);
+
+            });
         });
-    });
-
-    // Movimiento automático del carrusel
-    setInterval(() => {
-        currentIndex = (currentIndex + 1) % slides.length;
-        sliderNav(currentIndex);
-    }, 5000); // cambia cada 5 segundos
-
